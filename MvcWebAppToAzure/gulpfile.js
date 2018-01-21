255b651c93c0cae15eecda41f6cfbe70263dc35a
Harish <harishreddy1410@gmail.com>
2018-01-21 10:06:49 +0530

    Add project files.

---
+++ MvcWebAppToAzure/gulpfile.js
@@ -0,0 +1,45 @@
+﻿/*
+This file is the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. https://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+// Less configuration
+var gulp = require('gulp');
+var less = require('gulp-less');
+var cleanCss = require('gulp-clean-css')
+
+//Task which compiles less to css
+gulp.task('less', function () {
+    gulp.src('wwwroot/css/customcss/style.less')
+        .pipe(less())
+        .pipe(gulp.dest(function (f) {
+            return f.base;
+        }))
+        .pipe(cleanCss({ compatability: 'ie8' }));
+    // gulp.start('minifycss')
+});
+
+//Gulp task to minify the css files 
+gulp.task("minifycss", function (f) {
+    return gulp.src('wwwroot/css/customcss/style.css')
+        .pipe(cleanCss({ compatability: 'ie8' }))
+        .pipe(gulp.dest(function (f) { return f.base; }))
+});
+
+//Gulp task to watch out the changes to less files inside folder customcss then compiles less to CSS and minifies generated files
+gulp.watch('wwwroot/css/customcss/*.less', () => {
+    gulp.src('wwwroot/css/customcss/style.less')
+        .pipe(less())
+        .pipe(gulp.dest(function (f) {
+            return f.base;
+        }))
+        .pipe(cleanCss({ compatability: 'ie9' }))
+        .pipe(gulp.dest(function (f) {
+            return f.base;
+        }));
+});
+
+
+gulp.task('default', ['less'], function () {
+    gulp.watch('*.less', ['less']);
+})
---
+++ MvcWebAppToAzure/wwwroot/js/custom/CustomScript.js
@@ -0,0 +1,25 @@
+﻿
+//Method used to hide page loading icon
+function HideLoadingIcon() {
+    $('.loader').hide();
+}
+
+//Method used to show page loading icon
+function ShowLoadingIcon() {
+    $('.loader').show();
+}
+
+(function () {
+    function updateDateTime() {
+        var date = new Date();
+
+        container.innerText = date.toLocaleTimeString();
+    }
+
+    var container = document.body.getElementsByClassName("clock")[0];
+
+    updateDateTime();
+    setInterval(updateDateTime, 1000);
+})();
+
+window.onload = HideLoadingIcon();
